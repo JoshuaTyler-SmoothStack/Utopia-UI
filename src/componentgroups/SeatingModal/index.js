ef3c838fde8279378b6b5e9e2ffa91fc73e51226
JoshuaTyler-SmoothStack <Joshua.Tyler@smoothstack.com>
2021-03-21 19:55:51 -0400

    Feature: Seating Modal

--- src/componentgroups/SeatingModal/index.js
+++ src/componentgroups/SeatingModal/index.js
@@ -1,13 +1,14 @@
 // Libraries
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Store from "../../reducers/Store";
 
 // Components
 import Modal from "../../components/Modal";
 import FlexRow from "../../components/FlexRow";
 import FlexColumn from "../../components/FlexColumn";
-import AirplanesDispatcher from "../../dispatchers/AirplanesDispatcher";
+import KitUtils from "../../kitutils/KitUtils_v1.0.0";
 
+const ALPHABET = [ "A", "B", "C", "D", "E", "F", "G", "H"];
 const ZINDEX_DEFAULT = 2;
 
 const SeatingModal = (props) => {
@@ -16,29 +17,15 @@ const SeatingModal = (props) => {
   const align = props.align || "center";
   const background = props.background || "kit-bg-smoke-light";
   const zIndex = props.zIndex || ZINDEX_DEFAULT;
+  const [seatingTable, setSeatingTable] = useState(null);
 
   const handleRenderSeatingTable = () => {
-    AirplanesDispatcher.onRequestThenCallback(String(`/${flights.selected.flightAirplane.airplaneId}`),
-      (onError) => {
-      // alert the user their request cannot be processed right now
-      console.log("get airplane failed");
-    }, (onSuccess) => {
-      console.log(onSuccess);
-    });
-
-    console.log("handleRenderSeatingTable called");
+    
     console.log(airplanes.selected);
 
-    // const alphabet = [ "A", "B", "C", "D", "E", "F", "G", "H"];
+    
 
-    // const firstClassColumns = airplanes.selected;
-    // const firstClassRows = 3;
-    // const businessClassColumns = 6;
-    // const businessClassRows = 10;
-    // const economyClassColumns = 8;
-    // const economyClassRows = 100;
-    // const emergencyExitRows = [49, 50, 113];
-    // const occupiedSeats = { "1,1": true, "2,1": true, "4,1": true };
+
 
     // let firstClassItems = [];
     // // let businessClassItems = [];
@@ -80,6 +67,108 @@ const SeatingModal = (props) => {
     // );
     return <div></div>;
   };
+
+  const selectSeat = (seatPosition, isAvailable, isEmergencyExitRow) => {
+    if(!isAvailable) {
+      KitUtils.soundAlert();
+      // showErrorMessage(String(`Seat: ${seatPosition} is occupied`));
+    } else {
+      // setSelectingEmerencyExitRow(isEmergencyExitRow);
+      // setIsActiveConfirmSeatSelection(true);
+    }
+  };
+
+  useEffect(() => {
+    if(!seatingTable) {
+      setSeatingTable(true);
+      const firstClassCapacity = flights.selected.flightAirplane.airplaneType.airplaneTypeFirstClassCapacity;
+      const firstClassColumns = flights.selected.flightAirplane.airplaneType.airplaneTypeFirstClassColumns;
+      const firstClassRows = firstClassCapacity / firstClassColumns;
+      
+      const businessClassCapacity = flights.selected.flightAirplane.airplaneType.airplaneTypeBusinessClassCapacity;
+      const businessClassColumns = flights.selected.flightAirplane.airplaneType.airplaneTypeBusinessClassColumns;
+      const businessClassRows = businessClassCapacity / businessClassColumns;
+      
+      const coachClassCapacity = flights.selected.flightAirplane.airplaneType.airplaneTypeCoachClassCapacity;
+      const coachClassColumns = flights.selected.flightAirplane.airplaneType.airplaneTypeCoachClassColumns;
+      const coachClassRows = coachClassCapacity / coachClassColumns;
+      
+      const emergencyExitRows = flights.selected.flightAirplane.airplaneType.airplaneTypeEmergencyExitRows.split("-");
+      const occupiedSeats = ["1,1", "2,1", "4,1"];
+
+      const leftSeats = [];
+      const rightSeats = [];
+      // Loop Rows
+      const totalRows = (firstClassRows + businessClassRows + coachClassRows);
+      for(let rowIndex = 0; rowIndex < totalRows; rowIndex++) {
+        const rowLeftSide = [];
+        const rowRightSide = [];
+
+        let columnCount = coachClassColumns;
+        let defaultSeatColor = "kit-bg-bronze";
+        if(rowIndex < firstClassRows) {
+          defaultSeatColor = "kit-bg-gold";
+          columnCount = firstClassColumns;
+        }
+        else if(rowIndex < (firstClassRows + businessClassRows)) {
+          defaultSeatColor = "kit-bg-silver";
+          columnCount = businessClassColumns;
+        }
+
+        // Loop Columns
+        for(let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+
+          // Set Seat variables
+          let isAvailable = true;
+          let isEmergencyExitRow = false;
+          const seatPosition = String(`${rowIndex+1}${ALPHABET[columnIndex]}`);
+          let seatColor = defaultSeatColor;
+          if(occupiedSeats.includes(seatPosition)) {
+            seatColor = "btn-dark";
+            isAvailable = false;
+          }
+          else if(emergencyExitRows.includes(seatPosition)) {
+            seatColor = "btn-primary";
+            isEmergencyExitRow = true;
+          }
+
+          // Render the Seat
+          const selectedHalf = (columnIndex < (columnCount / 2)) ? rowLeftSide : rowRightSide;
+          selectedHalf.push(
+            <button key={String(`seat-${seatPosition}`)}
+              className={String(`btn m-1 btn-dark ${seatColor}`)}
+              style={{minHeight:"2rem", minWidth:"2rem"}}
+              onClick={() => selectSeat(seatPosition, isAvailable, isEmergencyExitRow)}
+            >
+              {seatPosition}
+            </button>
+          );
+        }
+
+        leftSeats.push(
+          <FlexRow key={String(`row-left-${rowIndex}`)} justify="around" wrap="no-wrap">
+            {rowLeftSide}
+          </FlexRow>
+        );
+        rightSeats.push(
+          <FlexRow key={String(`seat-${rowIndex}`)} justify="around" wrap="no-wrap">
+            {rowRightSide}
+          </FlexRow>
+        );
+      }
+
+      // Render rows of seats
+      const seatingTable = (
+        <FlexRow className="bg-white rounded" wrap="no-wrap">
+          <FlexColumn justify="around" wrap="no-wrap">{leftSeats}</FlexColumn>
+          <div className="bg-success rounded m-1" style={{height:String(`${totalRows}px`), width:"5px"}}/>
+          <FlexColumn justify="around" wrap="no-wrap">{rightSeats}</FlexColumn>
+        </FlexRow>
+      );
+      console.log(seatingTable);
+      setSeatingTable(seatingTable);
+    }
+  }, [airplanes, flights, seatingTable, setSeatingTable]);
 
   return (
     <Modal
@@ -135,8 +224,8 @@ const SeatingModal = (props) => {
               </div>
 
               {/* Seating Chart */}
-              <FlexRow className="col-12 mt-2">
-                {() => handleRenderSeatingTable()}
+              <FlexRow className="col-12 mt-2" style={{maxHeight:"50vh", overflowY: "scroll"}}>
+                {seatingTable}
               </FlexRow>
             </div>
           </div>
